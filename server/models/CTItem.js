852e8e02b7a25a113c9c1816e7d9c6c69a77b962
Gregg Robinson <grobinson@perceptron.com>
2021-08-17 16:01:35 -0400

    Fixed schema for CTItem

--- server/models/CTItem.js
+++ server/models/CTItem.js
@@ -1,5 +1,43 @@
 const mongoose = require("mongoose");
 
-const schema = require("./schemas/CTItemSchema");
+const schema = {
+  properties: {
+      serialNumber: {
+          type: String,
+          required: true
+      },
+      location: {
+          type: String
+      },
+      manufacturer: {
+          type: String
+      },
+      itemDescription: {
+          type: String
+      },
+      inService: {
+          type: Boolean,
+          required: true
+      },
+      model: {
+          type: String
+      },
+      itemGroup: {
+      type: String,
+      },
+      remarks: {
+          type: String
+      },
+      isStandardEquipment: {
+          type: Boolean
+      },
+      certificateNumber: {
+          type: String
+      },
+      timestamp: {
+          type: Date
+      }
+  }
+};
 
 module.exports = mongoose.model("CTItem", schema);
