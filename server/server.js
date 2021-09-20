758f34ba42e2a8132053670c63db0f5c2050b149
Gregg Robinson <grobinson@perceptron.com>
2021-09-20 12:59:31 -0400

    use x-forwarded-proto for heroku

--- server/server.js
+++ server/server.js
@@ -48,8 +48,7 @@ app.use((req, res, next) => {
     if(req.headers.host === host){
         if(!process.env.DEBUG){
             //console.log(req.headers);
-            if(!req.secure){
-                console.log(req.protocol);
+            if(req.get('x-forwarded-proto') === "http" && !req.secure){
                 //console.log(`https://${req.headers.host}${req.url}`);
                 return res.redirect(`https://${req.headers.host}${req.url}`);
             }
